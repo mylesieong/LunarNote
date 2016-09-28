047cf5d8c1702bff70aef945d68a59bea45c328f
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-09-28 16:10:55 +0800

    Add jt400 read dtaara

--- src/main/java/com/bcm/lunarnote/GreetingController.java
+++ src/main/java/com/bcm/lunarnote/GreetingController.java
@@ -5,6 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ibm.as400.access.*;
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
 /**
  * Controller of Spring MVC
  *
@@ -13,12 +19,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    //@RequestMapping("/greeting")
     @RequestMapping("/login")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        //return "greeting";
-        return "login";
+      /* read dtaara from as400 */
+      try{
+        String systemName="S657274B";
+        String userName="ZCSERVICE";
+        String password="ECIVRESCZ";
+        String fileName="DEMO_JAVA";
+        String libraryName="YMYLES";
+        String fileType="DTAARA";
+        AS400 system = new AS400(systemName, userName , password);
+        QSYSObjectPathName path = new QSYSObjectPathName(libraryName, fileName, fileType);
+        CharacterDataArea dataArea = new CharacterDataArea(system, path.getPath());
+        name = dataArea.read();
+      }catch(Exception e){
+        e.printStackTrace();
+        name = "No access";
+      }
+      model.addAttribute("name", name);
+      return "login";
     }
 
 }

67deab271b3ffab90202b6f1e66bfb3b2f698547
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-09-28 09:44:55 +0800

    create empty project

---
+++ src/main/java/com/bcm/lunarnote/App.java
@@ -0,0 +1,13 @@
+package com.bcm.lunarnote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

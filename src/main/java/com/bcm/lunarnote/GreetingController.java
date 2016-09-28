ccef04a384ad3a25039d3207050d1b78cac4417e
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-09-28 12:15:36 +0800

    Add application.java and controller.java: 1. Can run as runnable jar(with spring boot mvn plugin that search main) 2. Embed tomcat 8 inside 3. Open localhost:8080/greeting?name=myles to check result

---
+++ src/main/java/com/bcm/lunarnote/GreetingController.java
@@ -0,0 +1,22 @@
+package com.bcm.lunarnote;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Controller of Spring MVC
+ *
+ */
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}

844915a303dcd3873c3893ce6ece47ec29eb2301
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-09-28 15:26:57 +0800

    Add new login page in template

--- src/main/java/com/bcm/lunarnote/GreetingController.java
+++ src/main/java/com/bcm/lunarnote/GreetingController.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    //@RequestMapping("/greeting")
+    @RequestMapping("/login")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        //return "greeting";
+        return "login";
     }
 
 }
